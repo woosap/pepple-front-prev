873ae21e7ead576ff1143f553ea54c1b033187c4
leejiwonn <bbongwa@naver.com>
2021-04-13 06:45:26 +0900

    setting - Typescript / React / Next.js - eslint / prettier - emotion

---
+++ src/components/TestButton/TestButton.styles.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Button = styled.button`
+  padding: 10px 20px;
+  background: #aa4dcf;
+  color: #fff;
+  border: none;
+  border-radius: 10px;
+`;
